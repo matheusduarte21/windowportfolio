acdc97e9dbabc37aeda19ea9054f42c372e9fc1d
matheus duarte <matheus.duarte@everondata.com.br>
2024-11-13 16:40:30 -0300

    proteção

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -33,65 +33,65 @@ export const Footer = () => {
 
                 <div className="container-icons">
                     <div className="icon-with-dropdown">
-                        <img src={iconEdge} style={{ height: '30px', width: '30px', marginRight: '7px', marginLeft: '5px' }}/>
+                        <img alt='imagem'src={iconEdge} style={{ height: '30px', width: '30px', marginRight: '7px', marginLeft: '5px' }}/>
                         <div className="dropdown">
                             <div className="container-dropdown">
                                 <div className="container-title-dropdown">
                                 <div className="container-title-dropdown-icon">
-                                    <img src={iconEdge} style={{ height: '20px', width: '20px', display:'flex' }} />
+                                    <img alt='imagem' src={iconEdge} style={{ height: '20px', width: '20px', display:'flex' }} />
                                     </div>
                                     <p>Pesquisas devs - Egde</p>
                                 </div>
                                 <div className="container-imagem-dropdown">
-                                    <img src={imagemEdge} width='180px' height='100px'/>
+                                    <img alt='imagem' src={imagemEdge} width='180px' height='100px'/>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="icon-with-dropdown">
-                    <img src={iconVsCode} style={{ height: '30px', width: '30px', marginRight: '7px', marginLeft: '5px' }}/>
+                    <img alt='imagem' src={iconVsCode} style={{ height: '30px', width: '30px', marginRight: '7px', marginLeft: '5px' }}/>
                         <div className="dropdown">
                             <div className="container-dropdown">
                                 <div className="container-title-dropdown">
                                 <div className="container-title-dropdown-icon">
-                                <img src={iconVsCode} style={{ height: '20px', width: '20px', display:'flex' }} />
+                                <img alt='imagem' src={iconVsCode} style={{ height: '20px', width: '20px', display:'flex' }} />
                                     </div>
                                     <p>index.tsx - react projeto</p>
                                 </div>
                                 <div className="container-imagem-dropdown">
-                                    <img src={imagemVsCode} width='180px' height='100px'/>
+                                    <img alt='imagem' src={imagemVsCode} width='180px' height='100px'/>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="icon-with-dropdown">
-                        <img src={iconTeams} style={{ height: '30px', width: '30px', marginRight: '7px', marginLeft: '5px' }}/>
+                        <img alt='imagem' src={iconTeams} style={{ height: '30px', width: '30px', marginRight: '7px', marginLeft: '5px' }}/>
                         <div className="dropdown">
                             <div className="container-dropdown">
                                 <div className="container-title-dropdown">
                                 <div className="container-title-dropdown-icon">
-                                 <img src={iconTeams} style={{ height: '20px', width: '20px', display:'flex' }} />
+                                 <img alt='imagem' src={iconTeams} style={{ height: '20px', width: '20px', display:'flex' }} />
                                     </div>
                                     <p>Calendário - Matheus</p>
                                 </div>
                                 <div className="container-imagem-dropdown">
-                                    <img src={imagemTeams} width='180px' height='100px'/>
+                                    <img alt='imagem' src={imagemTeams} width='180px' height='100px'/>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="icon-with-dropdown">
-                    <img src={iconChrome} style={{ height: '30px', width: '30px', marginRight: '7px', marginLeft: '5px' }}/>
+                    <img  alt='imagem'src={iconChrome} style={{ height: '30px', width: '30px', marginRight: '7px', marginLeft: '5px' }}/>
                         <div className="dropdown">
                             <div className="container-dropdown">
                                 <div className="container-title-dropdown">
                                 <div className="container-title-dropdown-icon">
-                                <img src={iconChrome} style={{ height: '20px', width: '20px', display:'flex' }} />
+                                <img alt='imagem' src={iconChrome} style={{ height: '20px', width: '20px', display:'flex' }} />
                                     </div>
                                     <p>React App - Google</p>
                                 </div>
                                 <div className="container-imagem-dropdown">
-                                    <img src={imagemChrome} width='180px' height='100px'/>
+                                    <img alt='imagem' src={imagemChrome} width='180px' height='100px'/>
                                 </div>
                             </div>
                         </div>
